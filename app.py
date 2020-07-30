c7e1492071a3aa5e50659d92cba7d391eb41ce2e
Omar Miramontes <omar.miramontes@gmail.com>
2020-07-30 16:45:51 -0500

    Adds database manage y migrations

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-import os
+
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
+from models import setup_db
+
+import os
+
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
 
   return app
