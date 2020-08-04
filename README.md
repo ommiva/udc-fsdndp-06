f9f67a9e0f32b0752db3194e64f2b563dc3d38f8
Omar Miramontes <omar.miramontes@gmail.com>
2020-08-03 21:13:46 -0500

    Adds skeletons for actors and movies routes
    - Routes for movies and actors
      - GET, POST, PATCH, DELETE

--- README.md
+++ README.md
@@ -21,7 +21,8 @@ To create the database, first set DATABASE_URL environment variable
 export DATABASE_URL=postgres://postgres@[host]:5432/[database_name]
 ```
 
-Example:
+(TODO: Remove example)
+Example:   
 ```bash
 export DATABASE_URL=postgres://postgres@localhost:5432/casting_agency
 ```
@@ -36,3 +37,29 @@ To create (or update) tables, run
 python manage.py db migrate
 python manage.py db upgrade
 ```
+
+
+
+-------------------------------
+TODO: Remove debug
+-------------------------------
+### DEBUG
+* To install the application
+```bash
+$ pip install -r requirements.txt
+```
+
+* To run the application run the following command
+```bash
+export FLASK_APP=app.py
+export FLASK_ENV=development
+flask run
+```
+or
+```bash
+$ FLASK_APP=flaskr FLASKNV=development flask run
+```
+
+The API is run on http://127.0.0.1:5000/ by default.
+===============================
+
--- app.py
+++ app.py
@@ -17,5 +17,80 @@ def create_app(test_config=None):
 
 APP = create_app()
 
+
+# ROUTES
+@APP.route('/')
+def default_route():
+  print("Default route")
+  return jsonify({
+    "success": True
+  })
+
+
+@APP.route('/actors', methods=["POST"])
+def new_actor():
+  print("NEW actor")
+  return jsonify({
+    "success": True
+  })
+
+
+@APP.route('/actors-detail', methods=["GET"])
+def list_actors():
+  print("Actors list")
+  return jsonify({
+    "success": True
+  })
+
+
+@APP.route('/actors/<int:actor_id>', methods=["PATCH"])
+def update_actor(actor_id):
+  print("UPDATE actor")
+  return jsonify({
+    "success": True
+  })
+
+
+@APP.route('/actors/<int:actor_id>', methods=["DELETE"])
+def delete_actor(actor_id):
+  print("DELETE actor")
+  return jsonify({
+    "success": True
+  })
+
+
+@APP.route('/movies', methods=["POST"])
+def new_movie():
+  print("NEW movie")
+  return jsonify({
+    "success": True
+  })
+
+
+@APP.route('/movies-detail', methods=["GET"])
+def list_movies():
+  print("Movies list")
+  return jsonify({
+    "success": True
+  })
+
+
+@APP.route('/movies/<int:movie_id>', methods=["PATCH"])
+def update_movie(movie_id):
+  print("UPDATE movie")
+  return jsonify({
+    "success": True
+  })
+
+
+@APP.route('/movies/<int:movie_id>', methods=["DELETE"])
+def delete_movie(movie_id):
+  print("DELETE movie")
+  return jsonify({
+    "success": True
+  })
+
+
+
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
