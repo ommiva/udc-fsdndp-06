f425db911eb2f09fdf0bfe97df561b58ed659022
Omar Miramontes <omar.miramontes@gmail.com>
2020-08-05 14:00:02 -0500

    Fixes codestyle

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ import os
 
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  return app
+    return app
+
 
 APP = create_app()
 
@@ -21,93 +22,94 @@ APP = create_app()
 # ROUTES
 @APP.route('/')
 def default_route():
-  print("Default route")
-  return jsonify({
-    "success": True
-  })
+    print("Default route")
+    return jsonify({
+        "success": True
+    })
 
 
 @APP.route('/actors', methods=["POST"])
 def new_actor():
-  print("NEW actor")
-  return jsonify({
-    "success": True
-  })
+    print("NEW actor")
+    return jsonify({
+        "success": True
+    })
 
 
 @APP.route('/actors-detail', methods=["GET"])
 def list_actors():
-  print("Actors list")
-  return jsonify({
-    "success": True
-  })
+    print("Actors list")
+    return jsonify({
+        "success": True
+    })
 
 
 @APP.route('/actors/<int:actor_id>', methods=["PATCH"])
 def update_actor(actor_id):
-  print("UPDATE actor")
-  return jsonify({
-    "success": True
-  })
+    print("UPDATE actor")
+    return jsonify({
+        "success": True
+    })
 
 
 @APP.route('/actors/<int:actor_id>', methods=["DELETE"])
 def delete_actor(actor_id):
-  print("DELETE actor")
-  return jsonify({
-    "success": True
-  })
+    print("DELETE actor")
+    return jsonify({
+        "success": True
+    })
 
 
 @APP.route('/movies', methods=["POST"])
 def new_movie():
-  print("NEW movie")
-  return jsonify({
-    "success": True
-  })
+    print("NEW movie")
+    return jsonify({
+        "success": True
+    })
 
 
 @APP.route('/movies-detail', methods=["GET"])
 def list_movies():
-  print("Movies list")
-  return jsonify({
-    "success": True
-  })
+    print("Movies list")
+    return jsonify({
+        "success": True
+    })
 
 
 @APP.route('/movies/<int:movie_id>', methods=["PATCH"])
 def update_movie(movie_id):
-  print("UPDATE movie")
-  return jsonify({
-    "success": True
-  })
+    print("UPDATE movie")
+    return jsonify({
+        "success": True
+    })
 
 
 @APP.route('/movies/<int:movie_id>', methods=["DELETE"])
 def delete_movie(movie_id):
-  print("DELETE movie")
-  return jsonify({
-    "success": True
-  })
+    print("DELETE movie")
+    return jsonify({
+        "success": True
+    })
 
 
 # Error Handling
 
 @APP.errorhandler(422)
 def unprocessable(error):
-  return jsonify({
-            "success": False,
-            "error": 422,
-            "message": "Unprocessable"
-          }), 422
+    return jsonify({
+                "success": False,
+                "error": 422,
+                "message": "Unprocessable"
+            }), 422
+
 
 @APP.errorhandler(404)
 def not_found(error):
-  return jsonify({
-            "success": False,
-            "error": 404,
-            "message": "Resource not found"            
-          })
+    return jsonify({
+                "success": False,
+                "error": 404,
+                "message": "Resource not found"
+            })
 
 
 # ------------------------------------------------
--- models.py
+++ models.py
@@ -9,108 +9,108 @@ db_path = os.environ['DATABASE_URL']
 db = SQLAlchemy()
 
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 def setup_db(app, database_path=db_path):
+    '''
+    setup_db(app)
+        binds a flask application and a SQLAlchemy service
+    '''
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    
+
     db.app = app
     db.init_app(app)
     db.create_all()
 
 
 class Movie(db.Model):
-  '''
-  Movie
-  '''
+    '''
+    Movie
+    '''
 
-  __tablename__ = 'movies'
+    __tablename__ = 'movies'
 
-  id = Column(Integer, primary_key=True)
-  title = Column(String)
-  release_date = Column(Date)
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
 
-  def __init__(self, title, release_date):
-    self.title = title
-    self.release_date = release_date
-  
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
 
-  def update(self):
-    db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def update(self):
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'release_date': self.release_date
-    }
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-  def __repr__(self):
-    return f'<Movie {self.id} "{self.title}">'
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+    def __repr__(self):
+        return f'<Movie {self.id} "{self.title}">'
 
 
 class Actor(db.Model):
-  '''
-  Actor
-  '''
+    '''
+    Actor
+    '''
 
-  __tablename__ = 'actors'
+    __tablename__ = 'actors'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  age = Column(Integer)
-  gender = Column(String(15))
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String(15))
 
-  def __init__(self, name, age, gender):
-    self.name = name
-    self.age = age
-    self.gender = gender
-  
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
-  
-  def delete(self):
-    db.session.add(self)
-    db.session.commit()
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'age': self.age,
-      'gender': self.gender
-    }
-  
-  def __repr__(self):
-    return f'<Actor {self.id} "{self.name}" [a:{self.age}, g:{self.gender}]>'
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+    def __repr__(self):
+        return
+        f'<Actor {self.id} "{self.name}" [a:{self.age}, g:{self.gender}]>'
 
 
 class Cast(db.Model):
-  '''
-  Actors assigned to Movies
-  '''
+    '''
+    Actors assigned to Movies
+    '''
 
-  __tablename__ = 'movie_cast'
+    __tablename__ = 'movie_cast'
 
-  id = Column(Integer, primary_key=True)
-  actor_id = Column(Integer, ForeignKey('actors.id'), nullable=False)
-  movie_id = Column(Integer, ForeignKey('movies.id'), nullable=False)
+    id = Column(Integer, primary_key=True)
+    actor_id = Column(Integer, ForeignKey('actors.id'), nullable=False)
+    movie_id = Column(Integer, ForeignKey('movies.id'), nullable=False)
 
-  def __repr__(self):
-    return f'<Cast {self.id} actor: {self.actor_id} movie: {self.movie_id}>'
-
-
+    def __repr__(self):
+        return
+        f'<Cast {self.id} actor: {self.actor_id} movie: {self.movie_id}>'
