990b9bb5d72e5dfd3d33e24afb91e8c2e273c2d3
Omar Miramontes <omar.miramontes@gmail.com>
2020-09-22 11:15:27 -0500

    feat: env variables added to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
 
+export AUTH0_DOMAIN=omv-fsnd-casting.us.auth0.com
+export AUTH0_AUDIENCE=castingagency
+export AUTH0_CLIENT=v13pFbBPOlvhNdvmss6QLe13L2gpz9To
+export AUTH0_REDIRECT=http://localhost:8080/login-results
+
